f287ae3646e42cb374101dc85c743eb806c6da01
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-13 16:57:32 -0700

    W02D1 - Refactor eqArrays . Added new eqArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let emojiPass = String.fromCodePoint(0x1F603);
-  let emojiNotPass = String.fromCodePoint(0x1F621);
-  if (actual === expected) {
-    return `${emojiPass} Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `${emojiNotPass} Assertion Failed: ${actual} !=== ${expected}`;
-  }
-};
-// It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical.
-//takes two arrays and compare perfect match. returns boolean
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -22,11 +11,4 @@ const eqArrays = function(array1, array2) {
   } return true;
 };
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+module.exports = eqArrays;
