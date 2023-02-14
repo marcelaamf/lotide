5c41f3cd71f1ed2e3498d180851e336a7f05a035
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-13 17:20:13 -0700

    W02D01 Refacto assertArraysEqual, added assertArraysEqualTest file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,15 @@
-//  EQUAL_ARRAYS
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-};
-
+const eqArrays = require('./eqArrays');
 //ASSERT_EQUAL_ARRAYS
 let emojiPass = String.fromCodePoint(0x1F603);
 let emojiNotPass = String.fromCodePoint(0x1F621);
 const assertArraysEqual = function(actual, expected) {
-  if(eqArrays (actual, expected)) {
+  if (eqArrays(actual, expected)) {
     console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
+    return;
   } else {
     console.log(`${emojiNotPass} Assertion Failed: ${actual} !=== ${expected}`);
+    return;
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);// => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
+module.exports = assertArraysEqual;
